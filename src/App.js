7bf603e392237da26050aaaef98cb2e9b1cf59a5
jtreeves <jackson.t.reeves@gmail.com>
2020-12-10 11:46:00 -0500

    [App] Import components, and display them on page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
+import Input from './components/Input'
+import List from './components/List'
 
 function App() {
     return (
-        <div className='App'>
-            <header className='App-header'>
-                React App!
-            </header>
+        <div className='App-header'>
+            <h1>Fruit Filter</h1>
+            <Input />
+            <List />
         </div>
     )
 }
