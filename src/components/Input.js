2bb13dbf2c7fa23fabea58f0a6b2fedb4777d396
jtreeves <jackson.t.reeves@gmail.com>
2020-12-10 11:42:09 -0500

    [components] Fill in classes with constructors and renders

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 
 class Input extends Component {
+    constructor(props) {
+        super()
+        this.state = {
 
+        }
+    }
+    
+    render() {
+        return(
+            <div>
+                <h1>Input</h1>
+            </div>
+        )
+    }
 }
 
 export default Input
