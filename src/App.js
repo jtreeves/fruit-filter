003f2f0989111b88256884835423a151ab6595ab
jtreeves <jackson.t.reeves@gmail.com>
2020-12-10 12:27:56 -0500

    [App] Add array of fruits to feed into FruitContainer as props

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css'
 import FruitContainer from './components/FruitContainer'
 
 function App() {
+    const fruits = ['banana', 'watermelon', 'apple', 'orange']
+
     return (
         <div className='App-header'>
             <h1>Fruit Filter</h1>
-            <FruitContainer />
+            <FruitContainer fruits={fruits} />
         </div>
     )
 }
