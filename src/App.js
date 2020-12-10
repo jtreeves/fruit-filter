7a4a949482c8d911442b2bc51b10ff0cc1f1d628
jtreeves <jackson.t.reeves@gmail.com>
2020-12-10 16:20:48 -0500

    [class] Change App to class

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 // Import React's CSS
 import './App.css'
 
+// Import React's Component
+import React, { Component } from 'react'
+
 // Import main component
 import FruitContainer from './components/FruitContainer'
 
-// Create function for app
-function App() {
-    // Create array of fruits
-    const fruits = ['banana', 'watermelon', 'apple', 'orange', 'kiwi', 'crabapple', 'grape', 'grapefruit', 'mango', 'starfruit']
+// Create array of fruits
+const fruits = ['banana', 'watermelon', 'apple', 'orange', 'kiwi', 'crabapple', 'grape', 'grapefruit', 'mango', 'starfruit']
 
-    // Return this info to the page
-    return (
-        // Use 'App-header' styling
-        <div className='App-header'>
-            {/* Display title */}
-            <h1>Fruit Filter</h1>
-            {/* Send info to FruitContainer to use as props */}
-            <FruitContainer
-                // Send above fruits array as fruits
-                fruits={fruits}
-            />
-        </div>
-    )
+// Create class for app
+class App extends Component {
+    // Render page
+    render() {
+        // Return this info to the page
+        return (
+            // Use 'App-header' styling
+            <div className='App-header'>
+                {/* Display title */}
+                <h1>Fruit Filter</h1>
+                {/* Send info to FruitContainer to use as props */}
+                <FruitContainer
+                    // Send above fruits array as fruits
+                    fruits={fruits}
+                />
+            </div>
+        )
+    }
 }
 
 // Export app
